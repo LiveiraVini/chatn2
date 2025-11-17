8565d80960c24721dd7fb7802eb03ca19062835d
SENAC\vinicius59171446 <vini.oliveira02002@gmail.com>
2025-11-17 19:23:00 -0300

    teste codigo

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.get('/')
+def home():
+    teste = request
+    a = 10
+    b = 20
+    c = a + b
+    return f"A soma é {c}"
+
+@app.get('/login')
+def login():
+    return render_template('login.html')
+
+@app.post('/login')
+def realizarLogin():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return f"Email:{email}, Password: {password}"
+
+    if(email == 'teste@teste.com' and password == '123456'):
+        return 'login realizado com sucesso!'
+    else:
+        return 'Falha no login.'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
